f7ad77eda83aea8466d8e08a34705730be149453
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2019-03-07 15:00:21 +0000

    Update Cokcpit.js

--- src/component/Cockpit/Cokcpit.js
+++ src/component/Cockpit/Cokcpit.js
@@ -3,7 +3,7 @@ import React from "react";
 const cockpit = props => {
   return (
     <React.Fragment>
-      <h1 className={props.assingedClasses}>react app</h1>
+      <h1 className={props.assingedClasses} onClick={props.numbersClicked}>react app</h1>
       <button className={props.changeButton} onClick={props.clickChangeHandler}>
         click me{" "}
       </button>
