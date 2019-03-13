c736b4c16e7f78aca89673c525dddf3c2fdfe8ac
ermias kidane <hadeklte21@gmail.com>
2019-03-13 17:02:48 +0000

    remove the represantative of state in mobilpics.js

--- src/component/MobilePics/Mobilepics.js
+++ src/component/MobilePics/Mobilepics.js
@@ -4,25 +4,9 @@ import Iphone from "../../assets/images/iphone.jpg";
 import Huawei from "../../assets/images/huawei.jpg";
 import "./MobilePics.css";
 
-const roomMates = [
-  { label: "ermias" },
-  { label: "kokobe" },
-  { label: "natu" },
-  { label: "saymon" },
-  { label: "gdewon" },
-  { label: "cubo" }
-];
 const mobilePics = props => {
   return (
     <div className="MobilePics">
-      {roomMates.map(rm => {
-        return (
-          <ul key={rm.label}>
-            <li>{rm.label}</li>
-            <img className="Samsung" src={Iphone} alt="no display" />
-          </ul>
-        );
-      })}
       <img className="Samsung" src={Samsung} alt="no display" />
       <img className="Iphone" src={Iphone} alt="no display" />;
       <img className="Huawei" src={Huawei} alt="no display" />;
