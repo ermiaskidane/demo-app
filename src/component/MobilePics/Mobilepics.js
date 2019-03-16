511321085c889b1c7c2eec0273930ffabc1c5895
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2019-03-16 20:57:08 +0000

    Update Mobilepics.js

--- src/component/MobilePics/Mobilepics.js
+++ src/component/MobilePics/Mobilepics.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Samsung from "../../assets/images/samsung.jpeg";
 import Iphone from "../../assets/images/iphone.jpg";
 import Huawei from "../../assets/images/huawei.jpg";
 import "./MobilePics.css";
 
+class MobilePics extends Component {
+  render() {
+    console.log(this.props);
+    console.log(this.state.owner);
+    // though my state is an object it doesnot appear on the cleints ?
+    const transferredState = Object.keys(this.state.owner).map(o => {
+      return (
+        <ul key={o.id}>
+          <li>{o.name}</li>
+          <li>{o.type}</li>
+        </ul>
+      );
+    });
+    return (
+      <div className="MobilePics">
+        {transferredState}
+        <h1>hello:</h1>
+        <img className="Samsung" src={Samsung} alt="no display" />
+        <img className="Iphone" src={Iphone} alt="no display" />;
+        <img className="Huawei" src={Huawei} alt="no display" />;
+      </div>
+    );
+  }
+}
 
-
-const mobilePics = props => {
-  return (
-    <div className="MobilePics">
-      <img className="Samsung" src={Samsung} alt="no display" />
-      <img className="Iphone" src={Iphone} alt="no display" />;
-      <img className="Huawei" src={Huawei} alt="no display" />;
-    </div>
-  );
-};
-
-export default mobilePics;
+export default MobilePics;
